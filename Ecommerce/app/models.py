7e820b5767c3b8733a89a6ae4b15a94d85efd7c3
krunal <krunal.maru1106@gmail.com>
2022-12-16 13:14:02 +0530

    add image

--- Ecommerce/app/models.py
+++ Ecommerce/app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Slider(models.Model):
+    Discount_Deal = (
+        ('HOT DEALS','HOT DEALS'),
+        ('New DEALS','New DEALS')
+    )
+    image = models.ImageField(upload_to='media/slider_image')
+    discount_deal = models.CharField(choices=Discount_Deal, max_length=100)
+    brand_name = models.CharField(max_length=200)
+    sale = models.IntegerField()
+    discount = models.IntegerField()
+    link = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.brand_name
