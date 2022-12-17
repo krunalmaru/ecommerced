3d71c0bbfef8d58da5f3cb7f28084e6a71c27088
krunal <krunal.maru1106@gmail.com>
2022-12-17 13:05:33 +0530

    render product category

--- Ecommerce/app/views.py
+++ Ecommerce/app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from .models import Slider,Banner,Category,MainCategory,Subcategory
+from .models import Slider,Banner,Category,MainCategory,Subcategory,Product
 # Create your views here.
 def home(request):
     slider =  Slider.objects.all().order_by('-id')[0:3]
     banner = Banner.objects.all().order_by('id')[0:3]
     maincategory = MainCategory.objects.all()
-    context = {'slider':slider,'banner':banner, 'maincategory':maincategory}
+    product = Product.objects.filter(section__name = 'Top Selling Products')
+    context = {'slider':slider,'banner':banner, 'maincategory':maincategory,'product':product}
 
     return render(request, 'home/home.html',context)
