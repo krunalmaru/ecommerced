740bca7afab526bc0ae51b1cb8948bbb2fc952d8
krunal <krunal.maru1106@gmail.com>
2022-12-20 12:28:52 +0530

    password change using email

--- Ecommerce/Ecommerce/settings.py
+++ Ecommerce/Ecommerce/settings.py
@@ -191,5 +191,5 @@ LOGOUT_REDIRECT_URL = 'home'
 EMAIL_USE_TLS = True
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT =587
-EMAIL_HOST_USER ='Enter Your EmailId'
-EMAIL_HOST_PASSWORD = 'Enter Your Password'
+EMAIL_HOST_USER ='krunallm11@gmail.com'
+EMAIL_HOST_PASSWORD = 'ncvvoszbbdjslnlb'
